23cdfaec2bbef2b2bc4cd6ef1fa43c3083cd981e
Erika Espejo <erikaespejo95@gmail.com>
2021-06-27 19:49:38 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import List from './containers/List';
 import AuthForm from './containers/AuthForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -10,6 +16,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Container from '@material-ui/core/Container';
+import ListItem from '@material-ui/core/ListItem';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,26 +34,38 @@ function App() {
   const classes = useStyles();
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            React Twitter
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="sm">
-        <AuthForm />
-        <List />
-      </Container>
+      <Router>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <ListItem component={NavLink} to="/" button>
+              <Typography variant="h6" className={classes.title}>
+                React Twitter
+              </Typography>
+            </ListItem>
+            <ListItem component={NavLink} to="/login" button>
+              <Button color="inherit">Login</Button>
+            </ListItem>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="sm">
+          <Switch>
+            <Route path="/login">
+              <AuthForm />
+            </Route>
+            <Route path="/">
+              <List />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
     </>
   );
 }
