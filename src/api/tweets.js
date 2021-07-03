4bf29cfd57e78aa934a7c49df1bee79b14c32d31
Erika Espejo <erikaespejo95@gmail.com>
2021-07-03 17:21:04 -0500

    add axios, date-fns and refactor API

--- src/api/tweets.js
+++ src/api/tweets.js
@@ -1,23 +1,30 @@
-import { BASE_URL } from '../const';
+import http from '../utils/http';
+import { formatDistance } from 'date-fns';
+
+function formatTweet(tweet) {
+  const date = formatDistance(new Date(tweet.createdAt), new Date(), {
+    addSuffix: true,
+  });
+  return {
+    ...tweet,
+    id: tweet._id,
+    date,
+  };
+}
+
+function formatTweets(tweets) {
+  // return tweets.map((tweet) => formatTweet(tweet))
+  return tweets.map(formatTweet);
+}
 
 export const getTweets = async () => {
-  const token = localStorage.getItem('token');
-  const response = await fetch(`${BASE_URL}/tweets`, {
-    headers: {
-      Authorization: token,
-    },
-  });
-  const { data } = await response.json();
-  return data;
+  const response = await http.get(`/tweets`);
+  const { data } = response.data;
+  return formatTweets(data);
 };
 
 export const getTweet = async ({ id }) => {
-  const token = localStorage.getItem('token');
-  const response = await fetch(`${BASE_URL}/tweets/${id}`, {
-    headers: {
-      Authorization: token,
-    },
-  });
-  const { data } = await response.json();
-  return data;
+  const response = await http.get(`/tweets/${id}`);
+  const { data } = response.data;
+  return formatTweet(data);
 };
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import MuiAlert from '@material-ui/lab/Alert';
 import Tweet from '../components/Tweet';
 import API from '../api';
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 const List = () => {
   const [data, setData] = useState([]);
+  const [error, setError] = useState('');
   const history = useHistory();
 
   async function loadList() {
@@ -14,6 +20,7 @@ const List = () => {
         setData(data);
       }
     } catch (error) {
+      setError(error.message);
       console.log(error);
     }
   }
@@ -28,20 +35,20 @@ const List = () => {
 
   return (
     <>
-      {data.map((item) => {
-        const date = new Date(item.createdAt).toDateString();
+      {error && <Alert severity="error">{error}</Alert>}
+      {data.map(({ id, user, date, content }) => {
         return (
           <div
-            key={item._id}
+            key={id}
             onClick={() => {
-              displayTweet({ id: item._id });
+              displayTweet({ id });
             }}
           >
             <Tweet
-              name={item.user.name}
-              username={item.user.username}
+              name={user.name}
+              username={user.username}
               date={date}
-              content={item.content}
+              content={content}
             />
           </div>
         );
