e88c979006fb919c7ec124b95b38b14aed003c08
Erika Espejo <erikaespejo95@gmail.com>
2021-07-04 11:45:05 -0500

    add editUser and useUser hook

---
+++ src/containers/useUser.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import API from '../api';
+
+export default function useUser({ id }) {
+  const [user, setUser] = useState(null);
+
+  async function loadUser() {
+    try {
+      const data = await API.getUser({ id });
+
+      if (data) {
+        setUser(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if (id !== undefined) {
+      loadUser();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  return {
+    user,
+  };
+}
